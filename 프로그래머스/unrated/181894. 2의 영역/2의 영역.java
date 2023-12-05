51da1ba6b9dc53b7bc30606bb95407bc1d6bd174
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-05 15:49:30 +0900

    [level unrated] Title: 2의 영역, Time: 0.27 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] solution(int[] arr) {
+
+        int start = -1;
+        int end = arr.length - 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            if (arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+
+        if (start == -1) return new int[]{-1};
+        int[] answer = Arrays.copyOfRange(arr,start,end + 1);
+        return answer;
+    }
+}
