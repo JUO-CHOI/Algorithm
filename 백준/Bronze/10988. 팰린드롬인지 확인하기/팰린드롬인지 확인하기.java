721ec0d4606a193a623140d34afbeb28682c4a28
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-05 17:32:46 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();;
+        System.out.println(checkPalindrome(s));
+    }
+
+    public static int checkPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start <= end) {
+            if (s.charAt(start) != s.charAt(end)) return 0;
+            start++;
+            end--;
+        }
+        return 1;
+    }
+}
