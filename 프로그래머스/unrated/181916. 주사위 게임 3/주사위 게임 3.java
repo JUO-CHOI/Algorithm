5fa02629b07db2c9f368bef57c339c475377cd64
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-05 17:02:22 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.34 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -13,24 +13,22 @@ public static int solution(int a, int b, int c, int d) {
         // 세개만 같음
         else if (sortNum[0] == sortNum[2] || sortNum[1] == sortNum[3]) {
             // 앞 세개
-            if (sortNum[0] == sortNum[1]) return (10 * sortNum[0] + sortNum[3]) * (10 * sortNum[0] + sortNum[3]);
-            // 뒤 세개
-            else return (10 * sortNum[3] + sortNum[0]) * (10 * sortNum[3] + sortNum[0]);
+            // 이 경우 어차피 [1]과 [2]는 무조건 같음. [0]과 [3]을 더해서 [1]을 빼주면 다른 하나만 남는다.
+            return (int) Math.pow(sortNum[1] * 10 + (sortNum[0] + sortNum[3] - sortNum[1]), 2);
+
         }
+        // 두개씩 같음
         else if (sortNum[0] == sortNum[1] && sortNum[2] == sortNum[3]) {
             return (sortNum[0] + sortNum[3]) * (sortNum[3] - sortNum[0]);
         }
+        // 앞 두개만 같음 (뒤는 무조건 다름, 왜냐면 앞에서 같은 경우는 걸렀으니까)
+        else if (sortNum[0] == sortNum[1]) return sortNum[2] * sortNum[3];
+        // 가운데 두개만 같음
+        else if (sortNum[1] == sortNum[2]) return sortNum[0] * sortNum[3];
+        // 뒤 두개 같음
+        else if (sortNum[2] == sortNum[3]) return sortNum[0] * sortNum[1];
         // 네개 다 다름
-        else if (sortNum[0] != sortNum[1] && sortNum[1] != sortNum[2] && sortNum[2] != sortNum[3])
+        else 
             return sortNum[0];
-        // 두개만 같음
-        else {
-            // 앞 두개
-            if (sortNum[0] == sortNum[1]) return sortNum[2] * sortNum[3];
-            // 가운데 두개
-            else if (sortNum[1] == sortNum[2]) return sortNum[0] * sortNum[3];
-            // 뒤 두개
-            else return sortNum[0] * sortNum[1];
-        }
     }
 }
