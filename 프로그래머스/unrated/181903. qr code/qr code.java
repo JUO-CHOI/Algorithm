ae0947765b7ca875b26c2ebc87e5d0f44566b4fe
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-07 17:25:25 +0900

    [level unrated] Title: qr code, Time: 1.95 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -16,4 +16,15 @@ class Solution {
         String answer = str.toString();
         return answer;
     }
+    
+        public String solution_1(int q, int r, String code) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r)
+                str.append(code.charAt(i));
+        }
+        String answer = str.toString();
+        return answer;
+    }
 }
