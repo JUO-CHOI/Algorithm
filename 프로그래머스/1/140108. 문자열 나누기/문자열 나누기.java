5a3ae5773af1d8688276581ade473ebb8dd3b810
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-07 23:15:14 +0900

    [level 1] Title: 문자열 나누기, Time: 2.32 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,21 +1,25 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(String s) {
-        char x = s.charAt(0);
-        int answer = 1; // 앞 한덩어리 세고 시작
-        int countX = 0;
-        int notCountX = 0;
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        char compare = s.charAt(0);
+
         for (int i = 0; i < s.length(); i++) {
-            if (x == s.charAt(i)) countX++;
-            else notCountX++;
-            
-            if (countX == notCountX) {
-                if (i + 1 == s.length()) break; // 꽉 찼을 때 break, answer을 1부터 셌기 때문에 마지막 덩어리는 어차피 더해져 있음
-                answer++;
-                countX = 0;
-                notCountX = 0;
-                x = s.charAt(i+1);
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                compare = s.charAt(i);
+                count++;
+                continue;
+            }
+            if (compare == s.charAt(i)) {
+                stack.push(s.charAt(i));
+            }
+            else {
+                stack.pop();
             }
         }
-        return answer;
+        return count;
     }
 }
