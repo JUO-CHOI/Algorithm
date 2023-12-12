71d4d941c128621cfc3b844fa544f13909396de4
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-12 22:13:55 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -11,9 +11,27 @@ class Solution {
         for (int i = 0; i < arrayNum; i++) {
             int[] ijk = commands[i];
             int[] tempArr = Arrays.copyOfRange(array, ijk[0] - 1, ijk[1]);
-            Arrays.sort(tempArr);
+            int num = tempArr.length;
+            for (int j = 0; j < num; j++) {
+                int maxIndex = 0; // 가장 큰 수가 있는 인덱스
+                int temp;
+                // 배열의 길이만큼 비교, 제일 큰 수를 가장 뒤에 두므로
+                // 정렬할때마다 비교하는 횟수가 1씩 줄어듬
+                for(int k = 0; k < num - j; k++) {
+                    if (tempArr[k] > tempArr[maxIndex]) {
+                        maxIndex = k; // 더 큰 숫자를 가진 인덱스
+                    }
+                }
+                temp = tempArr[num - j - 1]; // 맨 뒷 숫자 temp에 저장
+                tempArr[num - j - 1] = tempArr[maxIndex]; // 맨 뒤에 가장 큰 수 저장
+                tempArr[maxIndex] = temp; // 빈 자리에 기존 맨 뒷 숫자 저장
+            }
+           
+//            Arrays.sort(tempArr);
             answer[i] = tempArr[ijk[2] - 1];
         }
+
+//        System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
