31e7353ed65052118dc95189f57a797328a8c40f
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-27 13:10:50 +0900

    [level 1] Title: 최소직사각형, Time: 1.32 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,18 @@
+class Solution {
+        public int solution(int[][] sizes) {
+        int widthMax = 0;
+        int heightMax = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            // 길이가 긴 걸 무조건 가로로 생각
+            if (sizes[i][0] >= sizes[i][1]) {
+                if (sizes[i][0] >= widthMax) widthMax = sizes[i][0];
+                if (sizes[i][1] >= heightMax) heightMax = sizes[i][1];
+            }
+            else {
+                if (sizes[i][1] >= widthMax) widthMax = sizes[i][1];
+                if (sizes[i][0] >= heightMax) heightMax = sizes[i][0];
+            }
+        }
+        return widthMax * heightMax;
+    }
+}
