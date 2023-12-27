a0a2d38d8c8561a912a1cb512c6525506102e636
juo <73274026+JUO-CHOI@users.noreply.github.com>
2023-12-27 14:38:09 +0900

    [level 1] Title: 모의고사, Time: 1.14 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,83 +1,33 @@
-import java.util.Arrays;
-
 class Solution {
-    public static int[] solution(int[] answers) {
-        int[] nums = {0, 0, 0};
+public static int[] solution(int[] answers) {
+        int[] student1 = {1, 2, 3, 4, 5};
+        int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int answer1 = 0;
+        int answer2 = 0;
+        int answer3 = 0;
 
         for (int i = 0; i < answers.length; i++) {
             int answer = answers[i];
-            // 1번 답 확인
-            if (checkNum1(i, answer)) {
-                nums[0]++;
-            }
-            if (checkNum2(i, answer)) {
-                nums[1]++;
-            }
-            if (checkNum3(i, answer)) {
-                nums[2]++;
-            }
+            if (answer == student1[i % student1.length]) answer1++;
+            if (answer == student2[i % student2.length]) answer2++;
+            if (answer == student3[i % student3.length]) answer3++;
         }
-        int max = Math.max(Math.max(nums[0], nums[1]), nums[2]);
 
+        int max = Math.max(Math.max(answer1, answer2), answer3);
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < 3; i++) {
-            if (nums[i] == max)
-                sb.append(i + 1);
-        }
-        String[] answerString = sb.toString().split("");
-        int[] answer = new int[answerString.length];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = Integer.parseInt(answerString[i]);
-        }
+        if (answer1 == max) sb.append(1);
+        if (answer2 == max) sb.append(2);
+        if (answer3 == max) sb.append(3);
 
+        String[] stringArr = sb.toString().split("");
+        int[] answer = new int[stringArr.length];
+        for (int i = 0; i < stringArr.length; i++) {
+            answer[i] = Integer.parseInt(stringArr[i]);
+        }
+        
         return answer;
-    }
-
-
-
-
-
-
-
-
-
-
-    public static boolean checkNum1(int i, int answer) {
-        return answer == i % 5 + 1;
-    }
-
-    public static boolean checkNum2(int i, int answer) {
-        // 홀수의 경우 무조건 2
-        if (i % 2 == 0) {
-            return answer == 2;
-        }
-        else {
-            int index = i % 8;
-            switch (index) {
-                case 1 : return answer == 1;
-                case 3 : return answer == 3;
-                case 5 : return answer == 4;
-                case 7 : return answer == 5;
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkNum3(int i, int answer) {
-        int index = i % 10;
-        switch (index) {
-            case 0 : return answer == 3;
-            case 1 : return answer == 3;
-            case 2 : return answer == 1;
-            case 3 : return answer == 1;
-            case 4 : return answer == 2;
-            case 5 : return answer == 2;
-            case 6 : return answer == 4;
-            case 7 : return answer == 4;
-            case 8 : return answer == 5;
-            case 9 : return answer == 5;
-        }
-        return false;
     }
 }
