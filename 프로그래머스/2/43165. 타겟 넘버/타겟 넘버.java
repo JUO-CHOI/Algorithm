87f9899444d17227ade797302124489e8c5466c0
juo <73274026+JUO-CHOI@users.noreply.github.com>
2024-01-04 15:40:26 +0900

    [level 2] Title: 타겟 넘버, Time: 0.40 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,31 @@
+public class Solution {
+    private static int count;
+
+    public static void main(String[] args) {
+        int[] numbers = {4,1,2,1};
+
+        System.out.println(solution(numbers, 4));
+    }
+
+    public static int solution(int[] numbers, int target) {
+        count = 0;
+        recursive(0, 0, numbers.length, numbers, target);
+
+        return count;
+    }
+
+    public static void recursive(int sum, int depth, int n, int[] numbers, int target) {
+        if (depth == n) {
+            if (sum == target) {
+                count++;
+            }
+        }
+        else {
+            sum += numbers[depth];
+            recursive(sum, depth + 1, n, numbers, target);
+            sum -= numbers[depth];
+            sum -= numbers[depth];
+            recursive(sum, depth + 1, n, numbers, target);
+        }
+    }
+}
