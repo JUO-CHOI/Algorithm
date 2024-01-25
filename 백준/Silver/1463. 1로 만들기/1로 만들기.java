0d3b45f30d743de923fe3efe3e13a4d74ad48f81
juo <73274026+JUO-CHOI@users.noreply.github.com>
2024-01-25 17:36:28 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -9,19 +9,19 @@ public class Main {
         int[] dp = new int[n + 1];
         dp[1] = 0;
         for (int i = 2; i <= n; i++) {
-            if (i % 3 == 0 && i % 2 == 0) {
-                dp[i] = Math.min(Math.min(dp[i / 3] + 1, dp[i / 2] + 1), dp[i - 1] + 1);
-                continue;
-            }
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i / 3] + 1, dp[i - 1] + 1);
-                continue;
-            }
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i / 2] + 1, dp[i - 1] + 1);;
-                continue;
-            }
             dp[i] = dp[i - 1] + 1;
+
+            if (i % 3 == 0 || i % 2 == 0) {
+                if (i % 3 == 0 && i % 2 == 0) {
+                    dp[i] = Math.min(Math.min(dp[i / 3] + 1, dp[i / 2] + 1), dp[i]);
+                }
+                else if (i % 3 == 0) {
+                    dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+                }
+                else {
+                    dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+                }
+            }
         }
         System.out.println(dp[n]);
     }
