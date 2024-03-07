0fd739cf23b49ae3660d091a6b23036f6e8e7a08
juo <73274026+JUO-CHOI@users.noreply.github.com>
2024-03-07 16:19:28 +0900

    [Silver II] Title: N번째 큰 수, Time: 972 ms, Memory: 342000 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,67 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // N 받기
         int n = Integer.parseInt(reader.readLine());
 
-        // 일렬로 세우고 mergesort 하자
-        int[] arr = new int[n * n];
-        int[] tmp = new int[n * n];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
 
-        // 입력 받기
-        int index = 0;
         for (int i = 0; i < n; i++) {
-            String[] inputLine = reader.readLine().split(" ");
+            String[] line = reader.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                arr[index] = Integer.parseInt(inputLine[j]);
-                index++;
+                priorityQueue.add(Integer.parseInt(line[j]));
             }
         }
 
-        mergeSort(0, arr.length - 1, arr, tmp);
-
-        System.out.println(arr[n -1]);
-    }
-
-    public static void mergeSort(int start, int end, int[] arr, int[] tmp) {
-        if (start < end) {
-            // 중앙값
-            int mid = (start + end) / 2;
-            // 앞쪽 정렬
-            mergeSort(start, mid, arr, tmp);
-            // 뒷쪽 정렬
-            mergeSort(mid + 1, end, arr, tmp);
-            // 병합
-            merge(start, mid, end, arr, tmp);
-        }
-    }
-
-    public static void merge(int start, int mid, int end, int[] arr, int[] tmp) {
-        for (int i = start; i <= end; i++) {
-            tmp[i] = arr[i];
-        }
-        int part1 = start;
-        int part2 = mid + 1;
-        int index = start;
-
-        while (part1 <= mid && part2 <= end) {
-            if (tmp[part1] >= tmp[part2]) {
-                arr[index] = tmp[part1];
-                part1++;
+        int index = 1;
+        while (true) {
+            if (index == n) {
+                System.out.print(priorityQueue.poll());
+                break;
             }
             else {
-                arr[index] = tmp[part2];
-                part2++;
+                priorityQueue.poll();
             }
             index++;
-        }
-
-        for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i] = tmp[part1 + i];
         }
     }
 }
