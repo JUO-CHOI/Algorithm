32409d395f2c48aebe4492db9ed67eaeb2d9dbd1
juo <73274026+JUO-CHOI@users.noreply.github.com>
2024-03-08 15:29:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.37 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,69 +2,49 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
+    private int n;
+    private int m;
+    private final int[] dx = {1, -1, 0, 0};
+    private final int[] dy = {0, 0, 1, -1};
 
-
-    // 좌표 이동을 위한 값. 순서대로 남북동서
-    private static final int[] dx = {1, -1, 0, 0};
-    private static final int[] dy = {0, 0, 1, -1};
-
-    private static int n;
-    private static int m;
-
-
-
-    public static void main(String[] args) {
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
-
-        System.out.println(solution(maps));
-    }
-
-
-    public static int solution(int[][] maps) {
+    public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
+        int[][] visited = new int[n][m];
 
-        return bfs(0, 0, maps);
-    }
-
-    public static int bfs(int x, int y, int[][] maps) {
-        int answer = -1;
-
-        // BFS에 사용할 큐
         Queue<int[]> queue = new LinkedList<>();
-        // 탐색완료를 나타낼 배열
-        int[][] used = new int[n][m];
-
-        // 첫 칸 넣어주기
-        queue.add(new int[]{x, y, 1});
+        // x, y, count
+        queue.add(new int[]{0, 0, 1});
+        visited[0][0] = 1;
 
         while (!queue.isEmpty()) {
-            int[] temp = queue.poll();
-            int tempX = temp[0];
-            int tempY = temp[1];
-            int count = temp[2];
+            int[] nowSearch = queue.poll();
+            int nowX = nowSearch[0];
+            int nowY = nowSearch[1];
+            int nowCount = nowSearch[2];
+            // 탐색했다고 추가
 
-            // 종료조건
-            if (tempX == n - 1 && tempY == m - 1) {
-                // return count;
-                answer = count;
-                break;
+            // 종료조건 -> (n,m)에 도착
+            if (nowX == n - 1 && nowY == m - 1) {
+                return nowCount;
             }
 
-            // 사방 탐색
             for (int i = 0; i < 4; i++) {
-                // 범위 벗어나면 패스
-                if (tempX + dx[i] < 0 || tempX + dx[i] >= n || tempY + dy[i] < 0 || tempY + dy[i] >= m) {
+                // 다음 탐색할 좌표
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+                // 맵 밖은 패스
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
                     continue;
-                }
-                // 탐색 가능하면 큐에 넣기
-                if (maps[tempX + dx[i]][tempY + dy[i]] == 1 && used[tempX + dx[i]][tempY + dy[i]] == 0) {
-                    // 방문 완료
-                    used[tempX + dx[i]][tempY + dy[i]] = 1;
-                    queue.add(new int[]{tempX + dx[i], tempY + dy[i], count + 1});
+                // 갈 수 있다면 추가 (길이 있고, 탐색하지 않음)
+                if (maps[nextX][nextY] == 1 && visited[nextX][nextY] == 0) {
+                    visited[nextX][nextY] = 1;
+                    queue.add(new int[]{nextX, nextY, nowCount + 1});
                 }
             }
         }
-        return answer;
+
+        // 답을 찾지 못하고 큐가 비었다 -> 갈 수 없다
+        return -1;
     }
 }
