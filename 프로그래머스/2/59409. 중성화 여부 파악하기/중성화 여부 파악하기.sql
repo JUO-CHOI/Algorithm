7e8cbb4c9b7b27c4c6fcd29ef0bd9cbb4b351d70
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-02-25 17:06:15 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,18 @@
-SELECT
-    ANIMAL_ID,
+# SELECT
+#     ANIMAL_ID,
+#     NAME,
+#     CASE
+#         WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+#         ELSE 'X'
+#     END AS 중성화
+# FROM ANIMAL_INS
+# ORDER BY ANIMAL_ID ASC;
+
+SELECT 
+    ANIMAL_ID, 
     NAME,
-    CASE
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+    CASE 
+        WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
         ELSE 'X'
     END AS 중성화
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID ASC;
+FROM ANIMAL_INS;
