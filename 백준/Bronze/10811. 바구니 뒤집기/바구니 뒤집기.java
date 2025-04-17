13f1d1cfb2f177d35f747fd3c4b43615ab1b9637
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-04-17 13:17:35 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 208 ms, Memory: 18468 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,32 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
+    static int m;
+    static int n;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-        scanner.nextLine();
 
-        int[] arry = new int[N];
+        // M와 N 입력받기
+        String input = scanner.nextLine();
+        String[] mn = input.split(" ");
+        m = Integer.parseInt(mn[0]);
+        n = Integer.parseInt(mn[1]);
 
-        for (int i = 0; i < N; i++) {
-            arry[i] = i+1;
+        // N 크기의 바구니 배열 만들기
+        int[] baskets = new int[m];
+        for (int i = 0; i < m; i++) {
+            baskets[i] = i + 1;
         }
 
-        for (int i = 0; i < M; i++) {
-            int first = scanner.nextInt();
-            int second = scanner.nextInt();
-            scanner.nextLine();
-            for (int j = 0; j < (second - first + 1) / 2; j++) {
-                int temp = arry[second - 1 - j];
-                // System.out.println(temp);
-                arry[second - 1 - j] = arry[first -1 + j];
-                arry[first - 1 + j] = temp;
-            }
+        // N번만큼 수행하기
+        for (int i = 0; i < n; i++) {
+            input = scanner.nextLine();
+            mn = input.split(" ");
+            int from = Integer.parseInt(mn[0]);
+            int to = Integer.parseInt(mn[1]);
+
+            baskets = reverse(baskets, from, to);
         }
 
-        for (int i : arry) {
-            System.out.print(i + " ");
+        // 출력하기
+        for (int i = 0; i < m; i++) {
+            System.out.print(baskets[i] + " ");
         }
+    }
+
+    public static int[] reverse(int[] baskets, int from, int to) {
+        // 임시 배열 만들기
+        int[] reverseBaskets = new int[baskets.length];
+        for (int i = 0; i < reverseBaskets.length; i++) {
+            reverseBaskets[i] = baskets[i];
+        }
+
+        // 시행할 횟수
+        int count = to - from + 1;
+
+        // 값 뒤집기
+        for (int i = 0; i < count; i++) {
+            reverseBaskets[to - i - 1] = baskets[from + i - 1];
+        }
+
+        return reverseBaskets;
     }
 }
