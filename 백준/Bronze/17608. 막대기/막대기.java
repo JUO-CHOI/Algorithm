3ef24bc959ff8d021cc3530eed85e47414f8d827
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-04-22 09:25:34 +0900

    [Bronze II] Title: 막대기, Time: 204 ms, Memory: 23236 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // N 받기
-        int n = Integer.parseInt(reader.readLine());
-        // N개의 정수 받기
-        int[] sticks = new int[n];
-        Stack<Integer> stickStack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            int stickLength = Integer.parseInt(reader.readLine());
-            stickStack.push(stickLength);
-        }
-        System.out.println(visibleStick(stickStack));
-    }
 
-    public static int visibleStick(Stack<Integer> stack) {
-        int count = 0;
-        int height = 0;
-        int now = 0;
-        while (!stack.empty()) {
-            now = stack.pop();
-            if (now > height) {
-                height = now;
-                count++;
+        // N 입력받기
+        int n = Integer.parseInt(reader.readLine());
+        // N개의 배열 만들기
+        int[] sticks = new int[n];
+
+        // 막대기 입력받기
+        for (int i = 0; i < n; i++) {
+            sticks[i] = Integer.parseInt(reader.readLine());
+        }
+
+        int answer = 0;
+        int max = 0;
+
+        // 오른쪽부터 검사하기
+        // 그동안 나왔던 막대보다 더 긴 막대만 보임
+        for (int i = 0; i < n; i++) {
+            if (sticks[n - i - 1] > max) {
+                max = sticks[n - i - 1];
+                answer++;
             }
         }
-        return count;
+
+        System.out.println(answer);
     }
 }
