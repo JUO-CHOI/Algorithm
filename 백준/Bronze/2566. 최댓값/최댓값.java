749de99f2b49ba37b2bf0f08d4256ac57c4ade50
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-04-22 11:08:48 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -5,23 +5,25 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int max = Integer.MIN_VALUE;
-        int xIndex = 0;
-        int yIndex = 0;
 
+        int row = 0;
+        int column = 0;
+        int max = -1;
+
+        // 배열 채워넣기
         for (int i = 0; i < 9; i++) {
-            String[] strNum = reader.readLine().split(" ");
+            String[] inputLineArray = reader.readLine().split(" ");
             for (int j = 0; j < 9; j++) {
-                int num = Integer.parseInt(strNum[j]);
-                if (num > max) {
-                    xIndex = i;
-                    yIndex = j;
-                    max = num;
+
+                if (Integer.parseInt(inputLineArray[j]) > max) {
+                    max = Integer.parseInt(inputLineArray[j]);
+                    row = i + 1;
+                    column = j + 1;
                 }
             }
         }
 
         System.out.println(max);
-        System.out.println(String.format("%d %d", xIndex + 1, yIndex + 1));
+        System.out.println(row + " " + column);
     }
 }
