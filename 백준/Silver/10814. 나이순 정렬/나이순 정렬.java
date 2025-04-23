0279f922454753f6db0dc0c11291bce282a6efb2
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-04-23 09:32:26 +0900

    [Silver V] Title: 나이순 정렬, Time: 620 ms, Memory: 54524 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,72 +5,84 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
 
-        // 나이, 이름 저장 (나이는 우선 String으로 저장 후 비교 시 int로 변환
-        String[][] members = new String[n][2];
-        String[][] tmp = new String[n][2];
+        int n = Integer.parseInt(reader.readLine());
+        Person[] members = new Person[n];
+        Person[] temp = new Person[n];
 
         for (int i = 0; i < n; i++) {
             String[] input = reader.readLine().split(" ");
-            members[i][0] = input[0];
-            members[i][1] = input[1];
+            int age = Integer.parseInt(input[0]);
+            String name = input[1];
+
+            members[i] = new Person(age, name);
         }
 
-        mergeSort(members, tmp, 0, n - 1);
+        mergeSort(members, temp, 0, members.length - 1);
 
         StringBuilder sb = new StringBuilder();
 
-        for (String[] line : members) {
-            sb.append(line[0]).append(" ").append(line[1]).append("\n");
+        for (Person member : members) {
+            sb.append(member.age).append(" ").append(member.name).append("\n");
         }
-
-        System.out.println(sb.toString());
-
-        reader.close();
+        System.out.println(sb);
     }
 
-    public static void mergeSort(String[][] arr, String[][] tmp, int start, int end) {
+    public static void mergeSort(Person[] members, Person[] temp, int start, int end) {
         if (start < end) {
             int mid = (start + end) / 2;
-            mergeSort(arr, tmp, start, mid);
-            mergeSort(arr, tmp, mid + 1, end);
-            merge(arr, tmp, start, mid, end);
+            mergeSort(members, temp, start, mid);
+            mergeSort(members, temp, mid + 1, end);
+            merge(members, temp, start, mid, end);
         }
     }
 
-    public static void merge(String[][] arr, String[][] tmp, int start, int mid, int end) {
+    public static void merge(Person[] members, Person[] temp, int start, int mid, int end) {
+        // 우선 사용할 만큼 임시배열에 저장
         for (int i = start; i <= end; i++) {
-            tmp[i][0] = arr[i][0];
-            tmp[i][1] = arr[i][1];
+            temp[i] = members[i];
         }
-        int part1 = start;
-        int part2 = mid + 1;
-        int index = start;
 
+        int part1 = start; // 왼쪽 시작 인덱스
+        int part2 = mid + 1; // 오른쪽 시작 인덱스
+        int index = start; // 실제 값을 넣어줄 인덱스
+
+        // 서로 값 비교하며 넣어주기
         while (part1 <= mid && part2 <= end) {
-            // 나이가 같으면 앞 배열의 사람 넣어줌
-            if (Integer.parseInt(tmp[part1][0]) == Integer.parseInt(tmp[part2][0])) {
-                arr[index][0] = tmp[part1][0];
-                arr[index][1] = tmp[part1][1];
+            // 앞 배열 사람이 어리거나 나이가 같음
+            // 나이가 같으면 앞 배열 사람 넣어줌 -> 일단 앞에 있는 사람이 먼저 가입한 사람임
+            if (temp[part1].age <= temp[part2].age) {
+                members[index] = temp[part1];
                 part1++;
             }
-            // 어린사람 넣어줌
-            else if (Integer.parseInt(tmp[part1][0]) < Integer.parseInt(tmp[part2][0])) {
-                arr[index][0] = tmp[part1][0];
-                arr[index][1] = tmp[part1][1];
-                part1++;
-            }
+            // part2이 어린 사람
             else {
-                arr[index][0] = tmp[part2][0];
-                arr[index][1] = tmp[part2][1];
+                members[index] = temp[part2];
                 part2++;
             }
             index++;
         }
-        for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i][0] = tmp[part1 + i][0];
-            arr[index + i][1] = tmp[part1 + i][1];
+
+        // 남은거 넣어주기
+        while (part1 <= mid) {
+            members[index] = temp[part1];
+            part1++;
+            index++;
         }
+        while (part2 <= end) {
+            members[index] = temp[part2];
+            part2++;
+            index++;
+        }
+    }
+}
+
+class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 }
