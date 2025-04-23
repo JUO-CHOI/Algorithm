561d4c3bafc12451a2ed40987b998512019f90f8
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-04-23 10:25:28 +0900

    [Silver V] Title: 숫자 카드, Time: 736 ms, Memory: 133696 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Scanner scanner = new Scanner(System.in);
-        check(reader);
-        reader.close();
 
-    }
-
-    public static void check(BufferedReader reader) throws IOException {
         int n = Integer.parseInt(reader.readLine());
-        String cards = reader.readLine();
-        Set<String> cardsSet = new HashSet<>(Arrays.asList(cards.split(" ")));
+        String[] input1 = reader.readLine().split(" "); // 상근이가 가진 카드 입력
+        boolean[] s = new boolean[20000001];
+        Arrays.fill(s, false);
+
+        for (int i = 0; i < input1.length; i++) {
+            s[Integer.parseInt(input1[i]) + 10000000] = true;
+        }
 
         int m = Integer.parseInt(reader.readLine());
-        String targets = reader.readLine();
-        String[] targetsArray = targets.split(" ");
+        String[] input2 = reader.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
 
-        int[] result = new int[m];
-        Arrays.fill(result, 0);
-
-        for (int i = 0; i < m; i++) {
-            if (cardsSet.contains(targetsArray[i])) {
-                result[i] = 1;
+        for (int i = 0; i < input2.length; i++) {
+            if (s[Integer.parseInt(input2[i])+ 10000000]) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
             }
-            System.out.print(result[i] + " ");
         }
+
+        System.out.print(sb);
     }
 }
