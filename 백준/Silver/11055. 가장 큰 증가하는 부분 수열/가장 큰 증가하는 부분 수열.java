3463f2519f041d13f806606def79e1e95e3ceab6
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-05-21 10:53:56 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 120 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,38 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(reader.readLine());
-
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-
-        int[] sequence = new int[n]; // 수열을 담을 배열
-        int[] dp = new int[n]; // dp[i]는 i번째 수가 가장 큰 부분 수열의 합 (증가하기 때문에)
+        String[] inputSequence = reader.readLine().split(" ");
+        int[] sequence = new int[n];
         for (int i = 0; i < n; i++) {
-            sequence[i] = Integer.parseInt(st.nextToken());
+            sequence[i] =Integer.parseInt(inputSequence[i]);
         }
 
-        // dp 채우기
+        int[] dp = Arrays.copyOf(sequence, sequence.length);
+
         for (int i = 0; i < n; i++) {
-            dp[i] = sequence[i]; // 최소 합은 본인
             for (int j = 0; j < i; j++) {
-                if (sequence[j] < sequence[i]) {
+                if (sequence[i] > sequence[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + sequence[i]);
                 }
             }
         }
 
-        // 가장 큰 값 찾기
         int answer = 0;
-        for (int num : dp) {
-            if (num > answer) {
-                answer = num;
-            }
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[i]);
         }
-        System.out.println(answer);
+
+        System.out.print(answer);
     }
 }
