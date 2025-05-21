527f0126ed4e59523a8994351c2950f573748840
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-05-21 13:16:46 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 15148 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -6,29 +6,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(reader.readLine()); // 집 개수
-
-        int[][] costs = new int[n][3];
-        int[][] dp = new int[n][3];
+        int n = Integer.parseInt(reader.readLine());
+        int[][] cost = new int[n][3];
 
         for (int i = 0; i < n; i++) {
-            String[] costArr = reader.readLine().split(" ");
-            costs[i][0] = Integer.parseInt(costArr[0]);
-            costs[i][1] = Integer.parseInt(costArr[1]);
-            costs[i][2] = Integer.parseInt(costArr[2]);
+            String[] inputCost = reader.readLine().split(" ");
+
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(inputCost[j]);
+            }
         }
 
-        // 첫 집 dp 넣기
+        int[][] dp = new int[n][3];
+
+        // 첫 가격 채우기
         for (int i = 0; i < 3; i++) {
-            dp[0][i] = costs[0][i];
+            dp[0][i] = cost[0][i];
         }
 
+        // dp 채우기
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1] + costs[i][0], dp[i - 1][2] + costs[i][0]);
-            dp[i][1] = Math.min(dp[i - 1][0] + costs[i][1], dp[i - 1][2] + costs[i][1]);
-            dp[i][2] = Math.min(dp[i - 1][0] + costs[i][2], dp[i - 1][1] + costs[i][2]);
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0]; // 현재 집을 빨간색으로
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1]; // 현재 집을 초록색으로
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2]; // 현재 집을 파란색으로
         }
 
-        System.out.println(Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2])));
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            if (dp[n - 1][i] < answer) {
+                answer = dp[n - 1][i];
+            }
+        }
+
+        System.out.print(answer);
     }
 }
