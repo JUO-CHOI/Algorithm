c38f36765e59ca831e8f80f09e7acf35d983480a
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-06-04 14:57:31 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.java
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.java
@@ -6,31 +6,35 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
+        String input = reader.readLine();
 
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                stack.push(Character.getNumericValue(s.charAt(i)));
-            }
-            else {
-                stack.push(cal(stack.pop(), stack.pop(), s.charAt(i)));
-            }
-        }
-        System.out.println(stack.peek());
-    }
 
-    public static int cal(int b, int a, char op) {
-        switch (op) {
-            case '+' :
-                return a + b;
-            case '-' :
-                return a - b;
-            case '*' :
-                return a * b;
-            case '/' :
-                return a / b;
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                stack.push(Integer.parseInt(input.substring(i, i + 1)));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                char op = input.charAt(i);
+                switch (op) {
+                    case ('+') :
+                        stack.push(a + b);
+                        break;
+                    case ('-') :
+                        stack.push(a - b);
+                        break;
+                    case ('*') :
+                        stack.push(a * b);
+                        break;
+                    case ('/') :
+                        stack.push(a / b);
+                        break;
+                }
+            }
         }
-        return 0;
+
+        System.out.print(stack.pop());
     }
 }
