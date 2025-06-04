f184b3c837b052e7884411b56e06aaab5bcb9dfe
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-06-04 12:50:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -8,12 +8,14 @@ public class Main {
         int n = Integer.parseInt(reader.readLine());
 
         int[] dp = new int[n + 1];
+
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i -1] % 10007 + dp[i -2] % 10007;
+
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = (dp[i - 2] % 10007) + (dp[i - 1] % 10007);
         }
 
-        System.out.println(dp[n] % 10007);
+        System.out.print(dp[n] % 10007);
     }
 }
