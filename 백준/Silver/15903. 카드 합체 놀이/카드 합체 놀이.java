7f5347e8c25be8ae000629db5b059158044d176f
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-06-12 13:21:12 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 140 ms, Memory: 15348 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -1,34 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] nm = reader.readLine().split(" ");
-        int n = Integer.parseInt(nm[0]);
-        int m = Integer.parseInt(nm[1]);
+        String[] inputMN = reader.readLine().split(" ");
+        String[] inputNums = reader.readLine().split(" ");
 
-        // 최종 합이 가장 작으려면 최대한 작은 수끼리 더해야 함
-        PriorityQueue<Long> priorityQueue = new PriorityQueue<>();
-        String[] cards = reader.readLine().split(" ");
+        int n = Integer.parseInt(inputMN[0]);
+        int m = Integer.parseInt(inputMN[1]);
+
+        int[] nums = new int[n];
+
         for (int i = 0; i < n; i++) {
-            priorityQueue.add(Long.parseLong(cards[i]));
+            nums[i] = Integer.parseInt(inputNums[i]);
         }
 
+        PriorityQueue<Long> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            priorityQueue.offer((long)nums[i]);
+        }
+
+        long temp1;
+        long temp2;
         for (int i = 0; i < m; i++) {
-            // 맨 앞에 두 카드 빼서 더한 다음 그 값을 두번 넣어줌 (두 카드에 값 덮어쓰기)
-            long num1 = priorityQueue.poll();
-            long num2 = priorityQueue.poll();
-            priorityQueue.add(num1 + num2);
-            priorityQueue.add(num1 + num2);
+            temp1 = priorityQueue.poll();
+            temp2 = priorityQueue.poll();
+
+            for (int j = 0; j < 2; j++) {
+                priorityQueue.offer(temp1 + temp2);
+            }
         }
 
-        long answer = 0;
+        long sum = 0;
         while (!priorityQueue.isEmpty()) {
-            answer += priorityQueue.poll();
+            sum += priorityQueue.poll();
         }
-        System.out.println(answer);
+
+        System.out.println(sum);
     }
 }
