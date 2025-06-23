bb72bb70bb744ba4d8e239929d70e886f7817b9d
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-06-23 13:03:28 +0900

    [Silver III] Title: N번째 큰 수, Time: 844 ms, Memory: 342412 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,33 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
         for (int i = 0; i < n; i++) {
-            String[] line = reader.readLine().split(" ");
+            String[] input = reader.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                priorityQueue.add(Integer.parseInt(line[j]));
+                priorityQueue.offer(Integer.parseInt(input[j]));
             }
         }
 
-        int index = 1;
-        while (true) {
-            if (index == n) {
-                System.out.print(priorityQueue.poll());
-                break;
-            }
-            else {
-                priorityQueue.poll();
-            }
-            index++;
+        int size = priorityQueue.size();
+
+        for (int i = 0; i < size; i++) {
+            int num = priorityQueue.poll();
+            if (i < n - 1) continue;
+            System.out.print(num);
+            break;
         }
     }
 }
