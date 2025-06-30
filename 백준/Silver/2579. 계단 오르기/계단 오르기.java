d56c393719b57ec0c8a528252307997f3a566582
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-06-30 13:08:39 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,28 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int n;
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
 
-        int[] stairs = new int[n + 1];
+        int[] points = new int[n + 1];
+        points[0] = 0;
         for (int i = 1; i <= n; i++) {
-            stairs[i] = Integer.parseInt(reader.readLine());
+            points[i] = Integer.parseInt(reader.readLine());
         }
 
-        int[] dp = new int[n + 1];
-
-        dp[1] = stairs[1];
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = points[1];
         if (n >= 2) {
-            dp[2] = stairs[1] + stairs[2];
+            dp[2] = dp[1] + points[2];
         }
 
         for (int i = 3; i <= n; i++) {
-            // 2계단 건넌 경우 or
-            // 한계단 건넌 경우(이땐 앞에서 연속이 아니어야 하므로 dp[i - 3]에서 두계단 건너 i - 1을 더하고 i를 더함)
-            dp[i] = Math.max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
+            // 한계단 오른 점수와 두계단 오른 점수 중 큰 값
+            // 한계단을 올랐다는건 전에는 두계단을 오른 것
+            dp[i] = Integer.max(dp[i - 3] + points[i - 1] + points[i], dp[i -2] + points[i]);
         }
 
         System.out.println(dp[n]);
