6017767d14a006634bc7aac8b7c29fbca64d6fed
juo <73274026+JUO-CHOI@users.noreply.github.com>
2025-07-02 10:04:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 808 ms, Memory: 102848 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,58 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
 
-        int[] numbers = new int[n];
-        int[] tmp = new int[n];
-
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(reader.readLine());
+            nums[i] = Integer.parseInt(reader.readLine());
         }
 
-        mergeSort(numbers, tmp, 0, n - 1);
+//        Arrays.sort(nums);
 
+        int[] temp = new int[n];
+
+        mergeSort(0, n - 1, nums, temp);
+        
         StringBuilder sb = new StringBuilder();
-
-        for (int num : numbers) {
+        for (int num : nums) {
             sb.append(num).append("\n");
         }
         System.out.print(sb.toString());
     }
 
-    public static void mergeSort(int[] arr, int[] tmp, int start, int end) {
+    public static void mergeSort(int start, int end, int[] array, int[] temp) {
         if (start < end) {
             int mid = (start + end) / 2;
-            mergeSort(arr, tmp, start,mid);
-            mergeSort(arr, tmp, mid + 1, end);
-            merge(arr, tmp, start, mid, end);
+            mergeSort(start, mid, array, temp);
+            mergeSort(mid + 1, end, array, temp);
+            merge(start, mid, end, array, temp);
         }
     }
 
-    public static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
-        for (int i = start; i <= end; i++) {
-            tmp[i] = arr[i];
-        }
+    public static void  merge(int start, int mid, int end, int[] array, int[] temp) {
+        int index = start;
         int part1 = start;
         int part2 = mid + 1;
-        int index = start;
-        while(part1 <= mid && part2 <= end) {
-            if (tmp[part1] < tmp[part2]) {
-                arr[index] = tmp[part1];
+
+        for (int i = start; i <= end; i++) {
+            temp[i] = array[i];
+        }
+
+        while (part1 <= mid && part2 <= end) {
+            if (temp[part1] <= temp[part2]) {
+                array[index] = temp[part1];
                 part1++;
-            }
-            else {
-                arr[index] = tmp[part2];
+            } else {
+                array[index] = temp[part2];
                 part2++;
             }
             index++;
         }
-        for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i] = tmp[part1 + i];
+
+        while (part1 <= mid) {
+            array[index] = temp[part1];
+            part1++;
+            index++;
         }
     }
 }
